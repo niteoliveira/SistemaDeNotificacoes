9c2c3ef435bb12a960c8ba3a2bd595020fb0631f
niteoliveira <toodigital2023@gmail.com>
2026-06-22 12:01:12 -0300

    Criacao do worker que escuta as notificações

--- SistemaNotificacoes.Worker/Worker.cs
+++ SistemaNotificacoes.Worker/Worker.cs
@@ -1,24 +1,47 @@
-namespace SistemaNotificacoes.Worker
+using System.Net.Http.Json;
+using SistemaNotificacoes.API;
+
+namespace SistemaNotificacoes.Worker;
+
+public class Worker : BackgroundService
 {
-    public class Worker : BackgroundService
+    private readonly ILogger<Worker> _logger;
+    private readonly HttpClient _httpClient;
+
+    public Worker(ILogger<Worker> logger)
     {
-        private readonly ILogger<Worker> _logger;
+        _logger = logger;
+        _httpClient = new HttpClient();
+    }
 
-        public Worker(ILogger<Worker> logger)
-        {
-            _logger = logger;
-        }
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        _logger.LogInformation("O Motor de Notificações foi iniciado.");
 
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        while (!stoppingToken.IsCancellationRequested)
         {
-            while (!stoppingToken.IsCancellationRequested)
+            try
             {
-                if (_logger.IsEnabled(LogLevel.Information))
+                _logger.LogInformation("Buscando notificações agendadas na API...");
+                var urlApi = "https://localhost:7123/api/notificacoes/agendar";
+
+                var notificacoes = await _httpClient.GetFromJsonAsync<List<Notificacao>>(urlApi, stoppingToken);
+
+                if (notificacoes != null && notificacoes.Any())
                 {
-                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                    _logger.LogWarning($"Encontrada(s) {notificacoes.Count} notificação(ões) na fila.");
                 }
-                await Task.Delay(1000, stoppingToken);
+                else
+                {
+                    _logger.LogInformation("Nenhuma notificação encontrada.");
+                }
             }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Erro ao conectar na API: {ex.Message}");
+            }
+            await Task.Delay(5000, stoppingToken);
         }
     }
+
 }
